cf3e15a33f502459a83bc6a5a98e4eb71ed95e98
nicolequindara <cquindara@gmail.com>
2018-02-19 19:10:03 -0700

    Add project files.

---
+++ CallEmAll.Test/MaxDistanceTest.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CallEmAll;
+
+namespace CallEmAll.Test
+{
+    [TestClass]
+    public class MaxDistanceTest
+    {
+        /// <summary>
+        /// Tests the nominal case
+        /// </summary>
+        [TestMethod]
+        public void Nominal()
+        {
+            // Arrange
+            char[] letters = new char[] { 'g', 'b', 'c', 'j', 'b', 'd', 'h', 'a' };
+
+            // Act
+            int actual = Program.MaxDistance(letters);
+
+            // Assert in test attribute
+            // 7 characters between 'b' and 'j'
+            Assert.AreEqual<int>(7, actual);
+        }
+
+        /// <summary>
+        /// Tests the nominal case
+        /// </summary>
+        [TestMethod]
+        public void Nominal2()
+        {
+            // Arrange
+            char[] letters = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'z' };
+
+            // Act
+            int actual = Program.MaxDistance(letters);
+
+            // Assert in test attribute
+            // 24 characters bettwen 'a' and 'z'
+            Assert.AreEqual<int>(24, actual);
+        }
+
+        /// <summary>
+        /// Tests the nominal case
+        /// </summary>
+        [TestMethod]
+        public void DecrementedSortedList()
+        {
+            // Arrange
+            char[] letters = new char[] { 'z', 'y', 'a' };
+
+            // Act
+            int actual = Program.MaxDistance(letters);
+
+            // Assert in test attribute
+            // Should return 0 since no previous characters are larger than any element
+            Assert.AreEqual<int>(0, actual);
+        }
+
+        /// <summary>
+        /// Tests error case when input character array is null
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NullInputArgument()
+        {
+            // Arrange
+            char[] letters = null;
+
+            // Act
+            Program.MaxDistance(letters);
+
+            // Assert in test attribute
+        }
+
+        /// <summary>
+        /// Tests error case when input array has length less than 2
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InvalidLengthInputArgument()
+        {
+            // Arrange
+            char[] letters = new char[] { 'a' };
+
+            // Act
+            Program.MaxDistance(letters);
+            // Assert in test attribute
+        }
+
+        /// <summary>
+        /// Tests error case when an element of the input array is not a lower case character
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NonAlphaChars()
+        {
+            // Arrange
+            char[] letters = new char[] { '.' };
+
+            // Act
+            Program.MaxDistance(letters);
+        }
+
+        /// <summary>
+        /// Tests error case when an element of the input array is not a lower case character
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void UpperAlphaChars()
+        {
+            // Arrange
+            char[] letters = new char[] { 'A' };
+
+            // Act
+            Program.MaxDistance(letters);
+        }
+    }
+}
---
+++ CallEmAll/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CallEmAll
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            char[] letters = new char[] { 'g', 'b', 'c', 'j', 'b', 'd', 'h', 'a' };
+
+            Console.WriteLine(MaxDistance(letters));
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// The maximum distance for a pair of any two letters in some array a is defined as the largest difference between any a[i] and a[j]
+        /// where i less than j and a[i] comes before a[j] in the alphabet. 
+        /// Write a function in C# named MaxDistance (which takes array a as a parameter) 
+        /// that calculates and returns the largest distance between two letters in the array.
+        /// </summary>
+        /// <param name="letters">Array of characters with at least length of 2</param>
+        /// <returns>The largest difference between any a[i] and a[j] 
+        /// where i less than j and a[i] comes before a[j] in the alphabet</returns>
+        public static int MaxDistance(char[] letters)
+        {
+            // Input validation
+            if (letters == null) throw new ArgumentException("Input array is null");
+            if (letters.Length < 2) throw new ArgumentException("Input array must contain at least 2 characters");
+
+            // ASSUMPTION: Want to limit to lower case characters only
+            foreach(char c in letters)
+            {
+                if (c < 'a' || c > 'z') throw new ArgumentException("Letters must be lower case");
+            }                      
+            
+            // Using constant extra space
+            int maxDistance = 0;
+            char minChar = letters[0];
+
+            // For-loop is O(N) time
+            for(int i = 1; i < letters.Length; i++)
+            {
+                // Calculate this distance between (non-inclusive) this character and the smallest character seen previously in the array
+                int distance = letters[i] - minChar - 1;
+
+                if (distance >= maxDistance)
+                {
+                    maxDistance = distance;
+                    Console.WriteLine(string.Format("Distance between {0} - {1}: {2}", letters[i], minChar, distance));
+                }
+
+                // Update smallest character seen
+                if (letters[i] < minChar) minChar = letters[i];                
+            }
+
+            return maxDistance;
+        }
+    }
+}
